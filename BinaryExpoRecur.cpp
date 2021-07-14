fa21e43410e48ea77478b1f7c93706b8bb176d4f
Neha Saxena <61897573+Neha-Saxena15@users.noreply.github.com>
2021-07-14 16:31:15 +0530

    Add files via upload

---
+++ BinaryExpoRecur.cpp
@@ -0,0 +1,29 @@
+    
+            /* Binary Exponentiation.....
+            sometime we want to find the value of 2**20 type values which is very large ....
+			so condition are given to find ans %M.....where 
+            M = 1e9 7;  */
+#include<iostream>
+long M = 1e9 +7;
+using namespace std;
+int recur(int a,int b)
+   {
+       if(b==0)
+	      return 1;
+	   int res = recur(a,b/2);	      
+	 if(b&1)
+	     return (a*res*res %M)%M;
+	else
+	  return (res*res)%M;	    	
+   }
+   
+
+int main()
+{
+	int a,b;  // we want to find (a^b)%M
+	cout<<"enter the value of a and b to find (a^b)%M ...where M is 10^9 + 7";
+	cin>>a>>b;
+  int ans = recur(a,b);
+  cout<<ans;
+	return 0;
+}
